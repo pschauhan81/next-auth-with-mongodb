b21c7d7efa7891608939e430936263160a2169c9
pschauhan81 <ps8168372@gmail.com>
2026-02-24 12:56:30 +0530

    feat: add nodemailer types and implement email sending functionality

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,11 @@
+import {connect}  from '@/dbConfig/dbConfig'
+import User from '@/models/userModel'
+import {NextResponse, NextRequest} from 'next/server'
+
+connect()
+
+export async function POST(request:NextRequest){
+    
+}
+
+
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,10 +1,33 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 
-export const sendEmail = async({email , emailType, userId })=>
-{
-    
-}
+export const sendEmail = async ({email, emailType, userId }:any) => {
+  try {
 
+    // TODO configure mail for usage
 
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
 
+    const mailOptions = {
+        
+    from: 'pardeepingh',
+    to: email   ,
+    subject: emailType ===  "VERIFY" ? "Verify your email" :"Reset your pasword",
+    // text: "Hello world?", // Plain-text version of the message
+    html: "<b>Hello world?</b>", // HTML version of the message
+  }
 
+   const mailResponse = await transporter.sendMail(mailOptions)
+        return mailResponse
+
+  } catch (error:any) {
+    throw new Error(error.message)
+  }
+};
