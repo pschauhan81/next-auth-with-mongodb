cf472b7282d36e99f20a33b2ea7afaacadb87c3c
pschauhan81 <ps8168372@gmail.com>
2026-02-25 14:19:41 +0530

    fix: correct user existence check message and improve code formatting

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,52 +1,53 @@
-import {connect}  from '@/dbConfig/dbConfig'
-import User from '@/models/userModel'
-import {NextResponse, NextRequest} from 'next/server'
-import bcryptjs from 'bcryptjs'
-import {sendEmail} from  '@/helpers/mailer'
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
+
 
 connect()
 
-export async function POST(request:NextRequest){
+
+export async function POST(request: NextRequest){
     try {
         const reqBody = await request.json()
         const {username, email, password} = reqBody
-        // validation 
+
         console.log(reqBody);
 
+        //check if user already exists
         const user = await User.findOne({email})
 
         if(user){
-             return NextResponse.json({error: "User already exits "},{status:400})
+            return NextResponse.json({error: "User already exists"}, {status: 400})
         }
 
-        const salt = await bcryptjs.genSalt(10);
-        const hashedPassword = await bcryptjs.hash(password, salt);
+        //hash password
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
 
         const newUser = new User({
             username,
             email,
-            password:hashedPassword
+            password: hashedPassword
         })
 
         const savedUser = await newUser.save()
-        console.log(savedUser)
+        console.log(savedUser);
 
-         //send verification email
-         
-            await sendEmail({email, emailType: "VERIFY", userId:savedUser._id})
+        //send verification email
+
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
 
         return NextResponse.json({
-             message:"User registered successfully",
-             success:true,
-             savedUser
+            message: "User created successfully",
+            success: true,
+            savedUser
         })
+        
+        
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
 
-    } catch (error:any) {
-        return NextResponse.json({error:error.message},
-            {status:500}
-        )
     }
 }
-
-
-
