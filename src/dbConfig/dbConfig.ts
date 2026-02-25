50fcad07849fa5265dea79729dee0dc0805d206f
pschauhan81 <ps8168372@gmail.com>
2026-02-25 15:43:12 +0530

    chore: update user model schema and improve connection handling

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -1,23 +1,24 @@
-import mongoose from "mongoose"
+import mongoose from 'mongoose';
 
-export async function connect (){
+export async function connect() {
     try {
-        mongoose.connect(process.env.MONGO_URI!)
-        const connection = mongoose.connection
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
 
-        connection.on('connected',()=>{
-            console.log('MongoDB connected')
+        connection.on('connected', () => {
+            console.log('MongoDB connected successfully');
         })
 
-        connection.on('error', ()=>{
-            console.log("MongoDB connection error please make sure is up and running ")
-            process.exit()
+        connection.on('error', (err) => {
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+            process.exit();
         })
 
     } catch (error) {
-        console.log('something went wrong in connecting to db ')
-        console.log(error)
+        console.log('Something goes wrong!');
+        console.log(error);
+        
     }
+
+
 }
-
-
