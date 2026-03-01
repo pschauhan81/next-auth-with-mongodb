da45e15f1b009342cde17c1db3b87656c07197b8
pschauhan81 <ps8168372@gmail.com>
2026-03-01 12:09:34 +0530

    refactor user retrieval in POST request to include user data and remove unused imports

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -1,8 +1,6 @@
 import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
-import bcryptjs from "bcryptjs";
-import jwt from "jsonwebtoken"
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 
 connect()
@@ -10,6 +8,11 @@ connect()
 export async function POST(request: NextRequest){
     // extract data from token 
     const userId = await getDataFromToken(request)
-    User.findOne()
+    const user = User.findOne({_id:userId}).select("-password")
+    //check if there is no user 
+    return NextResponse.json({
+        message:"User found ",
+        data:user 
+    })
 }
 
