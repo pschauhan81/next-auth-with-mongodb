e1dc6c9b25618ad8a79a631ce4c9c61d86688835
pschauhan81 <ps8168372@gmail.com>
2026-03-03 15:31:53 +0530

    feat: implement email verification page with user feedback and error handling

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios"
 import {toast} from "react-hot-toast"
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 
 export default function SignuPage() {
@@ -65,12 +66,19 @@ export default function SignuPage() {
       <label htmlFor='username'>Password</label>
       <input
       className='p-2 border border-gary-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black  bg-white'
-       type="text" 
+       type="password" 
        value={user.password}
        onChange={(e)=>setUser({...user, password:e.target.value})}
        placeholder="Password" 
        />
 
+        <button 
+        onClick={onSignup}
+        className='p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600'
+        >
+          {buttonDisabled ? "No signup" : "Signup"}
+        </button>
+        <Link href="/login" >Visit login page</Link>
 
     </div>
   )
